509328b11b9434612514187d2a094e1acf503cf4
tanugulavyshnavi0718-a11y <tanugulavyshnavi0718@gmail.com>
2026-09-04 15:46:25 +0530

    Update API_BASE_URL to new endpoint

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,7 +1,7 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
 const API_BASE_URL = "http://a033efa1e8b794dbbabf1603dfc2634f-1968123503.us-west-2.elb.amazonaws.com"
 
-// const API_BASE_URL = "http://98.92.112.243:84"
+// const API_BASE_URL = "http://98.92.112.243:84";
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
